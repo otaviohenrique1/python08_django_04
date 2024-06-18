70f3348a99bcbc6be265d3973b49e1fde50d06f9
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-06-18 17:44:38 -0300

    commit 4

--- alura_space/apps/galeria/views.py
+++ alura_space/apps/galeria/views.py
@@ -37,7 +37,8 @@ def buscar(request):
         nome_a_buscar = request.GET["buscar"]
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
-    return render(request, "galeria/buscar.html", {"cards": fotografias})
+    return render(request, "galeria/index.html", {"cards": fotografias})
+    # return render(request, "galeria/buscar.html", {"cards": fotografias})
 
 
 def nova_imagem(request):
@@ -54,9 +55,25 @@ def nova_imagem(request):
     return render(request, "galeria/nova_imagem.html", {"form": form})
 
 
-def editar_imagem(request):
-    return render(request, "galeria/editar_imagem.html")
+def editar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    form=FotografiaForms(instance=fotografia)
+    if request.method == "POST":
+        form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Fotografia editada com sucesso!")
+            return redirect("index")
+    return render(request, "galeria/editar_imagem.html", {"form": form, "foto_id": foto_id})
 
 
-def deletar_imagem(request):
-    return render(request, "galeria/deletar_imagem.html")
+def deletar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, "Deleção feita com sucesso!")
+    return redirect("index")
+
+
+def filtro(request, categoria):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True, categoria=categoria)
+    return render(request, "galeria/index.html", {"cards": fotografias})
